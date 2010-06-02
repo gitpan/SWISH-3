8b9d327b28bfd45dfcf6c3e2f34d1e397bc47f1e
Peter Karman <perl@peknet.com>
2010-06-02 04:06:44 +0000

    Import of KARMAN/SWISH-3-0.08_04 from CPAN.
    gitpan-cpan-distribution: SWISH-3
    gitpan-cpan-version:      0.08_04
    gitpan-cpan-path:         KARMAN/SWISH-3-0.08_04.tar.gz
    gitpan-cpan-author:       KARMAN
    gitpan-cpan-maturity:     developer

--- lib/SWISH/3.pm
+++ lib/SWISH/3.pm
@@ -4,7 +4,7 @@ use 5.008_003;
 
 package SWISH::3;
 
-our $VERSION = '0.08_03';
+our $VERSION = '0.08_04';
 
 # set by libswish3 in swish.c but that happens after %ENV has been
 # initialized at Perl compile time.
@@ -262,6 +262,12 @@ prints the contents of SWISH::3::Data to stderr.
 
 =head1 CLASS METHODS
 
+=head2 new( I<args> )
+
+I<args> should be an array of key/value pairs. See SYNOPSIS.
+
+Returns a new SWISH::3 instance.
+
 =head2 xml2_version
 
 Returns the libxml2 version used by libswish3.
@@ -281,17 +287,12 @@ should be a positive integer representing a Unicode codepoint.
 
 This prints a report similar to the swish_isw.c example script.
 
-=head1 OBJECT METHODS
-
-=head2 new( I<args> )
-
-I<args> should be an array of key/value pairs. See SYNOPSIS.
-
-Returns a new SWISH::3 instance.
-
 =head2 slurp( I<filename> )
 
-Returns the contents of I<filename> as a scalar string.
+Returns the contents of I<filename> as a scalar string. May also
+be called as an object method.
+
+=head1 OBJECT METHODS
 
 =head2 parse( I<filename_or_filehandle_or_string> )
 
